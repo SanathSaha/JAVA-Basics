9fb6493ae17236981530c30dc13e2734d73b9ee2
SanathSaha <sanath.saha007@gmail.com>
2025-05-07 11:06:07 +0600

    Leap Year check

---
+++ src/test/java/me/sanathsaha/SumofDigits.java
@@ -0,0 +1,10 @@
+package me.sanathsaha;
+
+import java.util.Scanner;
+
+public class SumofDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
